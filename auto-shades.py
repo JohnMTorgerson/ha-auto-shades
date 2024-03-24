2511597fa8f1bafb72677ff94f5f11001fb4b988
John Torgerson <john.m.torgerson@gmail.com>
2024-03-24 15:10:01 -0500

    added 'up' and 'down' profiles (lists of revs to perform, in order) for each window

--- auto-shades.py
+++ auto-shades.py
@@ -7,7 +7,7 @@ import json
 import asyncio
 
 
-def run(dir): # dir indicates direction to move shade: 1 for up, -1 for down
+def run(dir): # dir indicates direction to move shade: 'up' for up, or 'down' for down
     window_info = get_window_info()
 
     GPIO.cleanup()
@@ -25,13 +25,26 @@ def run(dir): # dir indicates direction to move shade: 1 for up, -1 for down
 
 
 async def run_window(w,dir):
+    '''
+    moves window shade up or down
+
+    w -- the json object for the window to move (which includes which gpio pins to use for that window)
+        e.g. "dir_pin" : 23,
+             "pul_pin" : 24,
+             "ena_pin" : 25,
+             "steps_per_rev" : 1600,
+             "up" : [33,-1],
+             "down" : [-32]
+    dir -- a string which should be either "up" or "down" to indicate which profile to use
+    '''
+
+    if dir != "up" and dir != "down" :
+        raise ValueError("value of dir should be either 'up' or 'down'")
 
     dir_pin = w["dir_pin"]
     pul_pin = w["pul_pin"]
     ena_pin = w["ena_pin"]
     steps_per_rev = w["steps_per_rev"]
-    revs = w["revs"] * dir # the number of revs to bring the shade all the way up or all the way down (dir being either 1 or -1 to indicate direction up or down, respectively)
-
 
     GPIO.setup(dir_pin,GPIO.OUT)
     GPIO.setup(pul_pin,GPIO.OUT)
@@ -40,27 +53,30 @@ async def run_window(w,dir):
     GPIO.output(pul_pin,0)
     GPIO.output(ena_pin,0)
 
+    # loop through profile for this window
+    # the profile is a list of numbers of revolutions of the motor (positive for up, negative for down)
+    # and their are two profiles for every window (w["up"] and w["down"])
+    for revs in w[dir] :
+        print(f"revs == {revs}")
 
-    print(f"revs == {revs}")
+        steps = int(float(revs) * steps_per_rev)
+        if steps < 0:
+            GPIO.output(dir_pin,1)
+            steps = -steps
+        else:
+            GPIO.output(dir_pin,0)
 
-    steps = int(float(revs) * steps_per_rev)
-    if steps < 0:
-        GPIO.output(dir_pin,1)
-        steps = -steps
-    else:
-        GPIO.output(dir_pin,0)
+        print(f"steps == {steps}")
 
-    print(f"steps == {steps}")
+        for i in range(steps):
+            if i%steps_per_rev == 0 :
+                print(f"{i} -- {i/steps_per_rev}")
+            
+            GPIO.output(pul_pin, 0)
+            GPIO.output(pul_pin, 1)
+            time.sleep(0.001)
 
-    for i in range(steps):
-        if i%steps_per_rev == 0 :
-            print(f"{i} -- {i/steps_per_rev}")
-        
-        GPIO.output(pul_pin, 0)
-        GPIO.output(pul_pin, 1)
-        time.sleep(0.001)
-
-    print (f"{steps} -- {steps/steps_per_rev}")
+        print (f"{steps} -- {steps/steps_per_rev}")
 
     # turn off power to motor (do not clean up after)
     GPIO.setmode(GPIO.BCM)
@@ -73,14 +89,10 @@ def get_window_info():
     return window_info
 
 if __name__ == "__main__" :
-    dir = 1.414
-    while dir != 1 and dir != -1:
-        if dir != 1.414 :
-            print("Must enter either 1 or -1")
-        dir = input("Enter '1' for up, '-1' for down: ")
-        try:
-            dir = int(dir)
-        except:
-            dir = 0
+    dir = ""
+    while dir != "up" and dir != "down":
+        if dir != "" :
+            print("Must enter either 'up' or 'down")
+        dir = input("Enter 'up' or 'down': ")
 
     run(dir)
