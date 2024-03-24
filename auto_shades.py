41887b27e80107156b94ae76651e19c7236def62
John Torgerson <john.m.torgerson@gmail.com>
2024-03-24 16:22:28 -0500

    added 'name' field to window info, added logging to main script

--- auto_shades.py
+++ auto_shades.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import configure_logging
+logger = configure_logging.configure(__name__)
 import RPi.GPIO as GPIO
 import time
 import json
 import asyncio
 
-
 def run(dir): # dir indicates direction to move shade: 'up' for up, or 'down' for down
-    window_info = get_window_info()
+    if dir not in ["up","down"] :
+        logger.error(f"Attempting to run shades, but dir param must be either 'up' or 'down': received '{dir}'")
+        raise ValueError("value of dir should be either 'up' or 'down'")
+
+    logger.info(f"Running shades {dir}")
+
+    try:
+        window_info = get_window_info()
+    except Exception as e:
+        logger.error(f"When trying to get window info: {repr(e)}")
+        raise e
 
     # GPIO.cleanup()
     GPIO.setwarnings(False)
@@ -18,6 +29,7 @@ def run(dir): # dir indicates direction to move shade: 'up' for up, or 'down' fo
         # loop through each window controlled by this computer
         # and run each window shade (up or down) concurrently
         for window in window_info :
+
             w = window_info[window]
             task = asyncio.create_task(run_window(w,dir))
             await task
@@ -39,6 +51,9 @@ async def run_window(w,dir):
     dir -- a string which should be either "up" or "down" to indicate which profile to use
     '''
 
+    name = w["name"]
+    logger.debug(f"Running {name} window")
+
     if dir != "up" and dir != "down" :
         raise ValueError("value of dir should be either 'up' or 'down'")
 
@@ -58,7 +73,7 @@ async def run_window(w,dir):
     # the profile is a list of numbers of revolutions of the motor (positive for up, negative for down)
     # and their are two profiles for every window (w["up"] and w["down"])
     for revs in w[dir] :
-        print(f"revs == {revs}")
+        logger.debug(f"{name} :: revs == {revs}")
 
         steps = int(float(revs) * steps_per_rev)
         if steps < 0:
@@ -67,17 +82,19 @@ async def run_window(w,dir):
         else:
             GPIO.output(dir_pin,0)
 
-        print(f"steps == {steps}")
+        logger.debug(f"{name} :: steps == {steps}")
 
         for i in range(steps):
             if i%steps_per_rev == 0 :
-                print(f"{i} -- {i/steps_per_rev}")
+                logger.debug(f"{name} :: {i} -- {i/steps_per_rev}")
             
             GPIO.output(pul_pin, 0)
             GPIO.output(pul_pin, 1)
             time.sleep(0.001)
 
-        print (f"{steps} -- {steps/steps_per_rev}")
+        logger.debug(f"{name} :: {steps} -- {steps/steps_per_rev}")
+
+    logger.debug(f"{name} :: COMPLETE")
 
     # turn off power to motor (do not clean up after)
     GPIO.setmode(GPIO.BCM)
